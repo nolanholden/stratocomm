22513672c2869dc0f843a93216bdd066b737ac7d
Nolan Holden <nolanholden@users.noreply.github.com>
2017-07-27 20:40:55 -0400

    add constants header

---
+++ src/stratocomm/stratocomm/constants.h
@@ -0,0 +1,13 @@
+#ifndef RCR_STRATOCOMM_CONSTANTS_H_
+#define RCR_STRATOCOMM_CONSTANTS_H_
+
+namespace rcr {
+namespace stratocomm {
+
+// LED pin
+constexpr uint8_t kLedPin = (uint8_t)13;
+
+} // namespace stratocomm
+} // namespace rcr
+
+#endif // RCR_STRATOCOMM_CONSTANTS_H_

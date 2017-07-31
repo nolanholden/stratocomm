f666e9b6785660b86271c4a48ef99b4fa5b31c68
Nolan Holden <nolanholden@users.noreply.github.com>
2017-07-31 06:27:46 -0400

    add standardized octet parsing/creation

--- src/stratocomm/octet-array.h
+++ src/stratocomm/octet-array.h
@@ -3,55 +3,41 @@
 
 #include "constants.h"
 
-#include <utility>
-
 namespace rcr {
 namespace stratocomm {
-namespace octets {
+namespace octet_array {
 
-namespace {
-  // static duration buffer (to prevent allocation upon every call.)
-  // [kMaxPayloadLength + 1] to include null terminator '\0'
-  char static_char_buffer[kMaxPayloadLength + 1];
+// Get an octet array of the string content (including the null
+// terminator '\0') along with its length.
+constexpr uint8_t get_octets(const String& text, uint8_t* octets_buffer) {
+  auto length = text.length(); // without terminator
 
-  // Garuntee array (pointer) liftime for get_octets();.
-  uint8_t static_octets_buffer[kMaxPayloadLength];
-} // namespace
+  for (auto i = 0u; i < length; ++i) {
+    octets_buffer[i] = static_cast<uint8_t>(text.charAt(i));
+  }
+  octets_buffer[length++] = '\0'; // add terminator
+
+  return static_cast<uint8_t>(length);
+};
 
 // Get a String from an octet array and its length. Trim either side as needed.
-String get_string(const uint8_t* const octets, uint8_t length,
+constexpr void get_string(const uint8_t* const octets, uint8_t length, char* buffer,
     uint8_t forward_offset = static_cast<uint8_t>(0),
     uint8_t offset_from_back = static_cast<uint8_t>(0)) {
   const uint8_t* const trimmed_octets = &(octets[forward_offset]);
   const uint8_t trimmed_length = length - forward_offset - offset_from_back;
   if (trimmed_length <= 0) {
-    auto s = String("");
-    return s;
+    buffer[0] = '\0';
+    return;
   }
   for (uint8_t i = 0; i < trimmed_length; ++i) {
-    static_char_buffer[i] = static_cast<char>(trimmed_octets[i]);
+    buffer[i] = static_cast<char>(trimmed_octets[i]);
   }
-  static_char_buffer[trimmed_length] = '\0'; // ensure strlen() will return correct length.
-  auto s = String(static_char_buffer);
-  return s;
-}
+  buffer[trimmed_length] = '\0'; // ensure strlen() will return correct length.
+  return;
+};
 
-// Get an octet array of the string content (including the null
-// terminator '\0') along with its length.
-// CATION: Garuntees array lifetime only until called again.
-std::pair<uint8_t*, uint8_t> get_octets(const String& text) {
-  auto length = text.length(); // without terminator
-
-  for (auto i = 0u; i < length; ++i) {
-    static_octets_buffer[i] = static_cast<uint8_t>(text.charAt(i));
-  }
-  static_octets_buffer[length++] = '\0'; // add terminator
-
-  auto pair = std::make_pair(static_octets_buffer, length);
-  return pair;
-}
-
-} // namespace octets
+} // namespace octet_array
 } // namespace stratocomm
 } // namespace rcr
 
